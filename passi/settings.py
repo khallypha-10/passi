97fb43fe0346be6f545cd739a3b7cb64c30ecaa8
sul <sul@gmail.com>
2023-07-11 06:39:27 +0300

    some changes

--- passi/settings.py
+++ passi/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,12 +81,8 @@ WSGI_APPLICATION = 'passi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'passif', 
-        'USER' : 'postgres',
-        'PASSWORD' : 'khallypha10', 
-        'HOST': 'localhost',
-        'PORT' : '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

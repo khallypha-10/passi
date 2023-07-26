2c2b1f6fd7b9f7c4c1b44304edb4a3b4d6715027
sul <sul@gmail.com>
2023-07-26 22:59:53 +0300

    new commit

--- pass/urls.py
+++ pass/urls.py
@@ -17,3 +17,4 @@ urlpatterns = [
     path('event-details<slug>', views.event, name="event"),
 ]
 urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)

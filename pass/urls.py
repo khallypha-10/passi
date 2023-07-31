ef25725cf2dd84e8149fbe6c8c0ca7ab22845017
sul <sul@gmail.com>
2023-07-31 17:47:50 +0300

    new commit

--- pass/urls.py
+++ pass/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     path('contact-us', views.contact, name="contact"),
     path('search', views.search, name="search"),
     path('events', views.events, name="events"),
-    path('donation', views.donation, name="donation"),
     path('initiate-payment/<slug>', views.initiate_payment, name='initiate_payment'),
     path('verify-payment/<str:ref>/', views.verify_payment, name='verify_payment'),
     path('event-details/<slug>', views.event, name="event"),

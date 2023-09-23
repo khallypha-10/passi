975fc0f4522c4c658d985726590469872b4fd7bb
sul <sul@gmail.com>
2023-09-23 15:17:36 +0300

    new commit

--- passi/settings.py
+++ passi/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'c!wbsq8w(93@*^cq6u&q75_=b@1c4b=ux5(hk0q4cb+qtt(^x!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['16.171.254.112', '127.0.0.1', 'www.passifoundation.com', 'passifoundation.com']
+ALLOWED_HOSTS = ['51.20.75.240', '127.0.0.1', 'www.passifoundation.com', 'passifoundation.com']
 
 # Application definition
 

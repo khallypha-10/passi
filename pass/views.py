e29083deda0abf3b99ef4310ad611f64ca80b2a0
sul <sul@gmail.com>
2024-12-13 15:08:48 +0300

    adjusted upcominng event

--- pass/views.py
+++ pass/views.py
@@ -14,7 +14,7 @@ def home(request):
     profiles = Member.objects.all()
     images = Image.objects.all()
     now = datetime.datetime.now()
-    upcoming = Event.objects.all().last()
+    upcoming = Event.objects.all().order_by('-id')[:1]
     total_causes = Cause.objects.all().count()
     people_helped = total_causes * 4000
    

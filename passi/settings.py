d3ff27f67fa63878381f69e00c8cfac75930c273
sul <sul@gmail.com>
2025-05-01 02:02:12 +0300

    new

--- passi/settings.py
+++ passi/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS')
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 

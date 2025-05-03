41460b865fe4de1e34bb7e52048b94a83b5ab167
sul <sul@gmail.com>
2025-05-03 20:53:05 +0300

    static images

--- passi/settings.py
+++ passi/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS')
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
